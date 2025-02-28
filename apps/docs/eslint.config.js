dc82242895f4e7b1c7fca003f36e2aaaf94d6e5e
Ajeje Brazorf <peppemini@gmail.com>
2025-02-28 17:12:58 +0100

    fix(docs): remove unnecessary dependecy

--- apps/docs/eslint.config.js
+++ apps/docs/eslint.config.js
@@ -1,8 +1,6 @@
 import { config } from '@repo/eslint-config/react'
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -14,16 +12,11 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
-  }
+  },
 )
